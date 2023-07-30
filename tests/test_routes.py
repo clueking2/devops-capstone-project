3cf6bb711de923dcf753a1c71dd4a84928a4f022
Claire Lueking <120064200+clueking2@users.noreply.github.com>
2023-07-30 08:40:00 -0500

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -131,12 +131,11 @@ class TestAccountService(TestCase):
     def test_get_account(self):
         """It should Read a single Account"""
         account = self._create_accounts(1)[0]
-        resp = self.client.get(
-            f"{BASE_URL}/{account.id}", content_type="application/json"
+        resp = self.client.get(f"{BASE_URL}/{account.id}", content_type="application/json"
         )
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
-        self.assertEqual(data["name"], "account.name")
+        self.assertEqual(data["name"], account.name)
 
     def test_update_account(self):
         """It should Update an existing Account"""
